8116f31879e1208a24e2fd660c26371ee711c85e
Rodrigo Mesquita <dev.rodrigomesquita@gmail.com>
2025-07-26 11:54:40 -0300

    queue algorithm

---
+++ queue.js
@@ -0,0 +1,50 @@
+class Queue {
+  constructor() {
+    this.items = []
+  }
+
+  enqueue(element) {
+    this.items.push(element)
+  }
+
+  dequeue() {
+    if (this.isEmpty() === 0) {
+      return 'Queue is empty'
+    }
+
+    return this.items.shift()
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return 'Queue is empty'
+    }
+
+    return this.items[0]
+  }
+
+  isEmpty() {
+    return this.items.length === 0
+  }
+
+  size() {
+    return this.items.length
+  }
+
+  clear() {
+    this.items = []
+  }
+
+  print() {
+    console.log(this.items.join(" -> "))
+  }
+}
+
+const queue = new Queue()
+
+queue.enqueue(10)
+queue.enqueue(5)
+queue.enqueue(2)
+queue.dequeue()
+queue.print()
+
