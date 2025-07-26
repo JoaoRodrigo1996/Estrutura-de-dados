ab6d42c64524ba8e66616aae69b49b3e287b3930
Rodrigo Mesquita <dev.rodrigomesquita@gmail.com>
2025-07-26 11:55:17 -0300

    selection sort algorithm

--- selection-sort.js
+++ selection-sort.js
@@ -3,19 +3,11 @@ function selectionSort(array){
     let lowIndex = i
 
     for(let j = i + 1; j < array.length; j++){
-      
-      console.log(`Comparando ${array[j]} com ${array[lowIndex]}`)
-
       if(array[j] < array[lowIndex]){
         lowIndex = j
       }
-
-      console.log(`Low index: ${array[lowIndex]} na posição ${lowIndex}`)
       
       if(lowIndex !== i){
-        console.log(`Trocando ${array[i]} com ${array[lowIndex]}`)
-        console.log('--------------------------')
-        
         let temp = array[i]
         array[i] = array[lowIndex]
         array[lowIndex] = temp
